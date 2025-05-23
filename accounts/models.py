fc0c9ef8438cba73787caec3b72913d16afdc920
Akisigure <daeone99@gmail.com>
2025-05-23 14:46:46 +0900

    [feat](auth):feature sign_up & login

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
 # Create your models here.
 
 class User(AbstractUser):
     pass
+    uuid = models.UUIDField(default=uuid.uuid4,unique=True,editable=False)
     gender = models.CharField(max_length=15)
     mbti = models.CharField(max_length=4)
     content = models.TextField(blank=True)
