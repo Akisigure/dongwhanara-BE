97b288f5e72a8dba245620d5307e072cda54bd71
Akisigure <daeone99@gmail.com>
2025-05-23 11:30:29 +0900

    [feat](books):book data API modified

--- books/utils.py
+++ books/utils.py
@@ -1,39 +1,15 @@
 from dotenv import load_dotenv
 import os
 import requests
-from .models import Book
 
-def get_save_book_data() :
+def get_data() :
     load_dotenv()
-    key = os.getenv('ALADIN_KEY')
-    start_cnt = 1
-    category_id = 48810
-    total_result = 0
-    call_cnt = 50
-    while True:
-        req = requests.get(f'http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey={key}&QueryType=ItemNewAll&MaxResults={call_cnt}&start={start_cnt}&SearchTarget=Book&CategoryId={category_id}&output=js&Version=20131101')
-        data = req.json()
-        start_idx = data['startIndex']
-        api_cnt = data['totalResults']
-        page = data['itemsPerPage']
+    key = os.getenv('SERVICE_KEY')
+    URL = f'http://api.kcisa.kr/openapi/API_LIB_048/request?serviceKey={key}&numOfRows=10000&pageNo=1'
+    headers = {'Content-Type' : 'application/json', 'Accept' : 'application/json'}
+    res = requests.get(URL,headers=headers)
 
-        if total_result == 0:
-            total_result = api_cnt
-        print(api_cnt)
-        print(start_idx)
+    #data
+    data = res.json()['response']['body']['items']['item']
 
-        total_result -= page
-
-        start_cnt += 1
-
-        if total_result <= 0:
-            break
-
-        for i in range(len(data['item'])):
-            print(data['item'][i]['title']) 
-            book = Book()
-            book.title = data['item'][i]['title']
-            book.save()
-
-
-
+    return data
