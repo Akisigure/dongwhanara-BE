75f8095a5ef48e91bcfa6ad4946714a7d5d75ebb
Akisigure <daeone99@gmail.com>
2025-05-23 23:39:25 +0900

    refactor:modified url & external api data permission modified

--- books/urls.py
+++ books/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test/',views.get_save_book_data),
-    path('list/',views.book_list),
+    path('import-external/',views.get_save_book_data),
+    path('/',views.book_list),
+    
 ]
