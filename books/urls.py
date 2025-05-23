b8f620968b4460b061e55ce6901aa97365df32e7
Akisigure <daeone99@gmail.com>
2025-05-24 00:16:06 +0900

    feat(books):feature book detail(GET) & modified swagger-ui

--- books/urls.py
+++ books/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('import-external/',views.get_save_book_data),
-    path('/',views.book_list),
-    
+    path('import-external/',views.get_save_book_data,name='import-external'),
+    path('',views.book_list),
+    path('<int:pk>/',views.book_detail)
 ]
