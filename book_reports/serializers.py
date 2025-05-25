636b8efcedb5aebe6183f7ab36280fa629552dce
Akisigure <daeone99@gmail.com>
2025-05-25 16:04:18 +0900

    feat(book_report):feature report list pagination

--- book_reports/serializers.py
+++ book_reports/serializers.py
@@ -9,5 +9,6 @@ class BookReportsSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ('book','user',)
 
+# GET 요청 생길때까지만 유지
 class CsrfmiddlewaretokenSerializer(serializers.Serializer):
     csrfmiddlewaretoken = serializers.CharField(max_length=64)
--- book_reports/views.py
+++ book_reports/views.py
@@ -7,6 +7,7 @@ from rest_framework.permissions import IsAdminUser,IsAuthenticated,IsAuthenticat
 from django.shortcuts import get_object_or_404
 from .models import BookReport
 from books.models import Book
+from .paginations import BookReportResultPagination
 
 @api_view(['GET','POST'])
 @permission_classes([IsAuthenticatedOrReadOnly])
@@ -15,27 +16,34 @@ def book_reports(request,book_pk):
 
     if request.method == 'GET':
         reports = BookReport.objects.filter(book=book)
-        serializer = BookReportsSerializer(reports, many=True)
-        return Response(serializer.data)
+        paginator = BookReportResultPagination()
+        result_page = paginator.paginate_queryset(reports,request)
+        serializer = BookReportsSerializer(result_page,many=True)
+        return paginator.get_paginated_response(serializer.data)
+    
     if request.method == 'POST':
         serializer = BookReportsSerializer(data = request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save(book=book,user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+
 @api_view(['GET','PUT','DELETE'])
 @permission_classes([IsAuthenticatedOrReadOnly])
 def report_detail(request,report_pk,book_pk):
     report = get_object_or_404(BookReport,pk = report_pk,book_id = book_pk)
+   
     if request.method == 'GET':
         serializer = BookReportsSerializer(report)
         return Response(serializer.data)
+    
     elif request.method == 'DELETE':
         if report.user == request.user:
             report.delete()
             return Response({'message':'삭제 성공.'},status=status.HTTP_204_NO_CONTENT)
         else:
             return Response({'message':'권한이 없습니다.'},status=status.HTTP_403_FORBIDDEN)
+    
     elif request.method == 'PUT':
         if report.user == request.user:
             serializer = BookReportsSerializer(report, data = request.data)
